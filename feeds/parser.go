37f75eff4ae3438341bb028f0c3f94b28a33c870
aadi. <aadi@aadi.net.in>
2023-11-06 14:44:18 +0530

    yes

--- feeds/parser.go
+++ feeds/parser.go
@@ -1,4 +1,6 @@
+// feeds/parser.go
 package feeds
+
 import (
 	"fmt"
 	"time"
@@ -6,8 +8,9 @@ import (
 	"github.com/mmcdole/gofeed"
 )
 
-func fetchAndParse(feedurl string) (*gofeed.Feed, error){
-parser := gofeed.NewParser()
+// fetchAndParse fetches and parses the RSS feed from the given URL.
+func fetchAndParse(feedURL string) (*gofeed.Feed, error) {
+	parser := gofeed.NewParser()
 	feed, err := parser.ParseURL(feedURL)
 	if err != nil {
 		return nil, fmt.Errorf("error fetching and parsing feed: %v", err)
@@ -15,6 +18,13 @@ parser := gofeed.NewParser()
 	return feed, nil
 }
 
+// handleFeedError prints a user-friendly error message based on the encountered error.
+func handleFeedError(err error) {
+	fmt.Println("Error:", err)
+	// You can add more specific error handling here if needed
+}
+
+// displayFeedContent displays the content of the parsed feed.
 func displayFeedContent(feed *gofeed.Feed) {
 	fmt.Printf("Title: %s\n", feed.Title)
 	fmt.Printf("Description: %s\n", feed.Description)
@@ -26,9 +36,11 @@ func displayFeedContent(feed *gofeed.Feed) {
 	}
 }
 
+// formatDate formats the date to a human-readable string.
 func formatDate(date *time.Time) string {
 	if date != nil {
 		return date.Format("2006-01-02 15:04:05")
 	}
 	return "N/A"
 }
+
